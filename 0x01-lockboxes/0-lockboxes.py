815082546713c5c798a1d1a407f996bbf7ad7305
Modeste Ciira <ciiramodeste23@gmail.com>
2023-12-05 14:17:07 +0300

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """ determines whether all the boxes in a
+    set of locked boxes can be opened"""
+    if not boxes or len(boxes) == 0:
+        return False
+    keys = [0]
+    visited_boxes = set(keys)
+    while keys:
+        current_box = keys.pop()
+        for key in boxes[current_box]:
+            if key < len(boxes) and key not in visited_boxes:
+                visited_boxes.add(key)
+                keys.append(key)
+    return len(visited_boxes) == len(boxes)
