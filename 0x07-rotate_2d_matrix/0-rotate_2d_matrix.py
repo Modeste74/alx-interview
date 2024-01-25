34992e728f5669acf082b42087f6d588bee9e5f5
Modeste74 <ciiramodeste23@gmail.com>
2024-01-25 06:37:37 -1200

    Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""defines a function rotate_2d_matrix"""
+
+
+def rotate_2d_matrix(matrix):
+    n = len(matrix)
+
+    for layer in range(n // 2):
+        first, last = layer, n - 1 - layer
+        for i in range(first, last):
+            offset = i - first
+            top = matrix[first][i]
+            matrix[first][i] = matrix[last - offset][first]
+            matrix[last - offset][first] = matrix[last][last - offset]
+            matrix[last][last - offset] = matrix[i][last]
+            matrix[i][last] = top
