bec3bc9adc3cd039206227ebc4f211f77777389f
Modeste74 <ciiramodeste23@gmail.com>
2024-02-01 06:11:47 -1200

    change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,4 +1,10 @@
+#!/usr/bin/python3
+"""defines a function makeCahnge"""
+
+
 def makeChange(coins, total):
+    """determine the fewest number of coins
+    needed to meet a given amount total"""
     if total <= 0:
         return 0
     dp = [float('inf')] * (total + 1)
